3f7e21d698090751715ac499b42e485c72865c48
antonycja <antonymaposa2002@gmail.com>
2022-02-12 22:50:37 +0200

    fixed menu title and made it dynamic

--- index.js
+++ index.js
@@ -1,56 +1,90 @@
-// window.addEventListener('scroll', function(){
-//     const header = document.querySelector('header nav');
-//     const subNav = document.querySelector('.sub-navigation nav');
-//     const subTitle = document.querySelector('.sub-navigation div h1')
-    
-//     header.classList.toggle('unset', window.scrollY <= 800);
-//     subNav.classList.toggle('fixed', window.scrollY >= 800);
-//     subTitle.classList.toggle('fixed', window.scrollY >= 800);
-// })
-
 const header = document.querySelector('header nav');
-const subNav = document.querySelector('.sub-navigation nav');
-const about = document.querySelector('.sub-navigation div h1');
-const projects = document.querySelector('#projects .title-tag h1');
-const moreProjects = document.querySelector('#more-projects div h1');
-const contact = document.querySelector('#contact div h1');
+const subNav = document.querySelector('.sub-navigation');
+const about = document.querySelector('.sub-navigation .about-tag h1');
+const projects = document.querySelector('.sub-navigation .projects-tag h1');
+const moreProjects = document.querySelector('.sub-navigation .more-projects-tag h1');
+const contact = document.querySelector('.sub-navigation .contact-tag h1');
 const sectionHeader = document.querySelector('header');
+const sectionAbout = document.querySelector('#about');
 const sectionProjects = document.querySelector('#projects');
 const sectionMoreProjects = document.querySelector('#more-projects');
 const sectionContact = document.querySelector('#contact');
 
-const sectionOptions = {
+const options = {
     rootMargin: "-15% 0% 0% 0%"
 };
+const newOptions = {
+    threshold:0.85,
+    rootMargin: "70% 0% 0% 0%"
+}
 
-const sectionHeaderObsever = new IntersectionObserver (
-    function(entries, sectionHeaderObsever){
+const HeaderObserver = new IntersectionObserver (
+    function(entries, HeaderObserver){
         entries.forEach(entry => {
             if (!entry.isIntersecting) {
                 header.classList.add('unset');
                 subNav.classList.add('fixed');
-                about.classList.add('fixed');
+                // about.classList.add('fixed');
             } else {
                 header.classList.remove('unset');
                 subNav.classList.remove('fixed');
+                // about.classList.remove('fixed');
+            }
+        });
+    },
+    options);
+    HeaderObserver.observe(sectionHeader)
+
+
+const aboutObserver = new IntersectionObserver (
+    function(entries, aboutObserver){
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                about.classList.add('fixed');
+            } else {
                 about.classList.remove('fixed');
             }
         });
     },
-    sectionOptions);
-    sectionHeaderObsever.observe(sectionHeader)
+    options);
+    aboutObserver.observe(sectionAbout)
 
-    const sectionProjectsObsever = new IntersectionObserver (
-        function(entries, sectionProjectsObsever){
-            entries.forEach(entry => {
-                if (!entry.isIntersecting) {
-                    about.classList.add('fixed');
-                    projects.classList.add('fixed');
-                } else {
-                    projects.classList.remove('fixed');
-                }
-            });
-        },
-        sectionOptions);
-        sectionProjectsObsever.observe(sectionProjects)
-    
+const projectsObserver = new IntersectionObserver (
+    function(entries, projectsObserver){
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                projects.classList.add('fixed');
+            } else {
+                projects.classList.remove('fixed');
+            }
+        });
+    },
+    newOptions);
+    projectsObserver.observe(sectionProjects)
+
+const moreProjectsObserver = new IntersectionObserver (
+    function(entries, moreProjectsObserver){
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                moreProjects.classList.add('fixed');
+            } else {
+                moreProjects.classList.remove('fixed');
+            }
+        });
+    },
+    newOptions);
+    moreProjectsObserver.observe(sectionMoreProjects)
+
+const contactObserver = new IntersectionObserver (
+    function(entries, contactObserver){
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                contact.classList.add('fixed');
+            } else {
+                contact.classList.remove('fixed');
+            }
+        });
+    },
+    newOptions);
+    contactObserver.observe(sectionContact)
+
